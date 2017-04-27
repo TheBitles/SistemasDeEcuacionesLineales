3ed38fb458c2836355aab7171a4ab54b53f0093c
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-04-27 17:44:20 -0300

    Se agrega la documentación del lote de prueba en la carpeta documentación y los archivos utilizados, y se actualiza la clase testSEL con métodos que prueben el caso del SCI y el SI

--- src/testsPackage/testSEL.java
+++ src/testsPackage/testSEL.java
@@ -6,36 +6,52 @@ import miPackage.SEL;
 
 public class testSEL {
 
+	// Todos los métodos de la clase SEL son testeados con el caso del SCI
 	@Test
 	public void testLevantaArchivo() {
 		@SuppressWarnings("unused")
-		SEL sel = new SEL("test/SEL/01_Ejemplo/entrada/sel.in");
+		SEL sel = new SEL("test/SEL/01_SistemaCompatibleDeterminado/entrada/sel.in");
 	}
 
 	@Test
 	public void testResuelveSistema() {
-		SEL sel = new SEL("test/SEL/01_Ejemplo/entrada/sel.in");
+		SEL sel = new SEL("test/SEL/01_SistemaCompatibleDeterminado/entrada/sel.in");
 		sel.resolver();
 	}
 
 	@Test
 	public void testCalculaErrorSolucion() {
-		SEL sel = new SEL("test/SEL/01_Ejemplo/entrada/sel.in");
+		SEL sel = new SEL("test/SEL/01_SistemaCompatibleDeterminado/entrada/sel.in");
 		sel.resolver();
 		sel.calcularErrorSolucion();
 	}
 	
 	@Test
 	public void testMostrarSolucion() {
-		SEL sel = new SEL("test/SEL/01_Ejemplo/entrada/sel.in");
+		SEL sel = new SEL("test/SEL/01_SistemaCompatibleDeterminado/entrada/sel.in");
 		sel.resolver();
 		sel.mostrarSolucion();
 	}
 
 	@Test
 	public void testEscribeSolucionEnArchivo() {
-		SEL sel = new SEL("test/SEL/01_Ejemplo/entrada/sel.in");
+		SEL sel = new SEL("test/SEL/01_SistemaCompatibleDeterminado/entrada/sel.in");
 		sel.resolver();
-		sel.escribirSolucionEnArchivo("test/SEL/01_Ejemplo/salida/solucion.out");
+		sel.escribirSolucionEnArchivo("test/SEL/01_SistemaCompatibleDeterminado/salida/solucion.out");
+	}
+	
+	// Con estos dos métodos se testean los casos de los sistemas SCI y SI
+	@Test
+	public void testSistemaCompatibleIndeterminado() {
+		SEL sel = new SEL("test/SEL/02_SistemaCompatibleIndeterminado/entrada/sel.in");
+		sel.resolver();
+		sel.escribirSolucionEnArchivo("test/SEL/02_SistemaCompatibleIndeterminado/salida/solucion.out");
+	}
+	
+	@Test
+	public void testSistemaIncompatible() {
+		SEL sel = new SEL("test/SEL/03_SistemaIncompatible/entrada/sel.in");
+		sel.resolver();
+		sel.escribirSolucionEnArchivo("test/SEL/03_SistemaIncompatible/salida/solucion.out");
 	}
 }
