1d5aecec97e9d01a2d28677a972eaff1f89fe924
Laboratorios <laboratorios@unlmlab.edu.ar>
2017-05-02 20:12:42 -0300

    Correciones de la entrega. Listo.

--- src/miPackage/Main.java
+++ src/miPackage/Main.java
@@ -5,25 +5,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Main {
-	
-	public static void resolverSistema() {
-		
-		System.out.println("SISTEMAS DE ECUACIONES LINEALES");
-		
-		System.out.println("El sistema se levanta de un archivo entrada.in en la ra�z del proyecto");
-		System.out.println("La soluci�n se graba en un archivo salida.out en la ra�z del proyecto");
-		
-		SEL sel = new SEL("sel.in");
-		borrarSolucionAnterior();
-		sel.resolver();
-		sel.escribirSolucionEnArchivo("solucion.out");
-		
-		System.out.println("Soluci�n del sistema");
-		sel.mostrarSolucion();
-		
-		System.out.println("Error cometido en la resoluci�n del sistema: " + sel.calcularErrorSolucion());
-	
-	}
 
 	public static void borrarSolucionAnterior() {
 		File arch = new File("solucion.out");
@@ -31,18 +12,33 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		
-		SEL.generarSELAleatorio(1000);
+
+		System.out.println("SISTEMAS DE ECUACIONES LINEALES");
+
+		System.out.println("El sistema se levanta de un archivo entrada.in en la ra�z del proyecto");
+		System.out.println("La soluci�n se graba en un archivo salida.out en la ra�z del proyecto");
+
+		SEL sel = new SEL("sel.in");
 		borrarSolucionAnterior();
-		
+
+		SEL.generarSELAleatorio(10);
+
 		Calendar tiempoInicial = new GregorianCalendar();
 
-		resolverSistema();
-		
+		sel.resolver();
+
 		Calendar tiempoFinal = new GregorianCalendar();
-		
+
 		long variacionTiempo = tiempoFinal.getTimeInMillis() - tiempoInicial.getTimeInMillis();
 		System.out.println("Rendimiento del algoritmo: " + variacionTiempo);
+
+		sel.escribirSolucionEnArchivo("solucion.out");
+
+		System.out.println("Soluci�n del sistema");
+		sel.mostrarSolucion();
+
+		System.out.println("Error cometido en la resoluci�n del sistema: " + sel.calcularErrorSolucion());
+
 	}
 
 }
